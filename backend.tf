a0ddb1b659df5fc981d2b3305fc9e2985b65481c
Hala Elhamahmy <hala.elhamahmy@gmail.com>
2024-10-22 08:31:40 +0300

    update

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     # Replace this with your bucket name!
     bucket         = "terraform-up-and-running-state"
     key            = "global/s3/terraform.tfstate"
-    region         = var.region
+    region         = "us-east-1"
 
     # Replace this with your DynamoDB table name!
     dynamodb_table = "terraform-up-and-running-locks"

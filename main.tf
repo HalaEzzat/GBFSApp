c188caa48dbf763af1c33ae615eef46d03c9dac2
Hala Elhamahmy <hala.elhamahmy@gmail.com>
2024-10-22 20:17:55 +0300

    update

--- main.tf
+++ main.tf
@@ -2,3 +2,29 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = "hala-elhamahmy"
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
+  bucket = "hala-elhamahmy"
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"  # AWS-managed key
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "secure" {
+  bucket = "hala-elhamahmy"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
